0b792fbc8524e6c901026890dee102d4fe5f78c4
joao pedro fonseca dantas <fonsecajope@gmail.com>
2020-08-03 11:51:59 -0300

    applying sascha's sidebar model
    but isn't working...

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const myName = "Jope"
-console.log(`Hello, it's me, ${myName}`)
+$(document).ready(function() {
+    $('.navigation_trigger, .navigation a').click(function() {
+        $('body').toggleClass('sidebar_is_active');
+    });
+});
+$(function() {
+    $('a[href*=\\#]:not([href=\\#])').click(function() {
+        if(location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.lenght) {
+                var targety = target.offset().top;
+                var realtargety = targety - 32;
+                $('html,body').animate({
+                    scrolltop: realtargety,
+                    }, 500, 'swing');
+                return false;
+            };
+        };
+    })
+});
